b9f73d40a2aa6e4d01ba56e2f159bc4beecfa122
Sevag <39042935+sevtam@users.noreply.github.com>
2026-05-16 12:48:42 +1000

    Update license notice to SPDX identifier and reference LICENSE file

--- rename-computer_ps1/rename-computer_v1.2.ps1
+++ rename-computer_ps1/rename-computer_v1.2.ps1
@@ -1,32 +1,15 @@
-# MIT License
-#
+# SPDX-License-Identifier: MIT
 # Copyright (c) 2026 Sevag Tamoukian
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# This script is licensed under the MIT License.
+# See the LICENSE file in the repository root for full license text.
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# ---
 # How to Use:
 # 1. Open PowerShell as Administrator.
 # 2. Run this script: .\rename-computer.ps1
 # 3. The computer will be renamed to 'LPT' plus the last 7 characters of the BIOS serial number (max 15 chars).
 # 4. Restart the computer to apply the new name.
-#
+
 $serialNumber = (Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber)
 
 $hostname = "LPT" + $serialNumber.Substring($serialNumber.Length - 7)
